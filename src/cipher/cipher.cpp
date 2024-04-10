0c97f4eb29bb359d20ed7d08bb8d0e6dce55e9e5
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-04-09 17:06:17 -0700

    Remove unecessary shift/key sign check.

--- src/cipher/cipher.cpp
+++ src/cipher/cipher.cpp
@@ -12,12 +12,6 @@ RetCode AsciiCaesarCipher(std::istream &is, std::ostream &os, int shift) {
     return RetCode::kBadOutputStream;
   }
 
-  /* Negative shifts get adjusted to be positive. The ciphertext winds up being
-   * the same as if we had directly applied the negative shift. */
-  if (shift < 0) {
-    shift += kAsciiAlphabetSize;
-  }
-
   char curr = '\0';
   while (is.get(curr)) {
     curr = (static_cast<int>(curr) + shift) % kAsciiAlphabetSize;
