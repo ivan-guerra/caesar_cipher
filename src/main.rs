289ad5774cbe9d7ea1768b1887ddcefc78c62da2
ivan-guerra <ivan.eduardo.guerra@gmail.com>
2024-11-09 09:27:16 -0500

    Add command line arg processing for the ccipher utility.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(help = "cipher encryption/decryption key")]
+    key: i32,
+
+    #[arg(short = 'i', long, help = "input plaintext/ciphertext file")]
+    input_file: Option<std::path::PathBuf>,
+
+    #[arg(short = 'o', long, help = "output plaintext/ciphertext file")]
+    output_file: Option<std::path::PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
